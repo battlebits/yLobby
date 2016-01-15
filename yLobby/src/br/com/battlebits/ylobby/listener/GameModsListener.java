3dee37a75bba76296f28e2085d9d72e575277663
zViitoor <rodrigues.victor@outlook.com>
2016-01-15 03:15:39 -0200

    Correcao de uma mensagem simples

--- yLobby/src/br/com/battlebits/ylobby/listener/GameModsListener.java
+++ yLobby/src/br/com/battlebits/ylobby/listener/GameModsListener.java
@@ -32,7 +32,7 @@ public class GameModsListener implements Listener {
 					} else if (type.equalsIgnoreCase("MATCH")) {
 						e.getPlayer().sendMessage("§0");
 						e.getPlayer().sendMessage(yLobbyPlugin.getyLobby().getzUtils().getMessageUtils()
-								.centerChatMessage("§9§lConectando §7ao servidor §9§l" + name + "§7!"));
+								.centerChatMessage("§9§lConectando §7ao §9§l" + name + "§7!"));
 						e.getPlayer().sendMessage("§0");
 						e.getPlayer().sendPluginMessage(yLobbyPlugin.getyLobby(), "BungeeCord",
 								new BungeeMessage(((String) e.getRightClicked().getMetadata("GM_CONNECT").get(0).value()).split("#")).getDataOutput()
@@ -64,7 +64,7 @@ public class GameModsListener implements Listener {
 						} else if (type.equalsIgnoreCase("MATCH")) {
 							p.sendMessage("§0");
 							p.sendMessage(yLobbyPlugin.getyLobby().getzUtils().getMessageUtils()
-									.centerChatMessage("§9§lConectando §7ao servidor §9§l" + name + "§7!"));
+									.centerChatMessage("§9§lConectando §7ao §9§l" + name + "§7!"));
 							p.sendMessage("§0");
 							p.sendPluginMessage(yLobbyPlugin.getyLobby(), "BungeeCord",
 									new BungeeMessage(((String) e.getEntity().getMetadata("GM_CONNECT").get(0).value()).split("#")).getDataOutput()
